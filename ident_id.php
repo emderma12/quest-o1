908421512e63aceab7a08044657bc79299f42e50
emderma12 <93205579+emderma12@users.noreply.github.com>
2021-12-14 15:23:59 -0300

    envio da questão 1

---
+++ ident_id.php
@@ -0,0 +1,9 @@
+<?php 
+$nome = $_GET['nome'];
+$idade = $_GET['idade'];
+$altura = $_GET['altura'];
+
+echo "NOME:".$nome."<br>";
+echo "idade:".$idade. "<br>";
+echo "altura".$altura. "<br>";
+ ?>
---
+++ questao1pt1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+<head>
+	<meta charset="utf-8">
+	<title> questão 1</title>
+</head>
+<body>
+
+<p><a href="ident_id.php?nome=maria&idade=18&ltura=1.7"> Passar valores</p></a>
+</html>
